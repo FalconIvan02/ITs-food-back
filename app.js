6e0be0eb530596c11587ee8dbecfb94a91459a4e
Ivan Falcon <Falconivanernesto@gmail.com>
2023-12-01 00:13:54 -0300

    vista previa terminada y CORS manejado(se puede mejorar)

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 import express, { json } from 'express'
 import { foodRouter } from './routes/foodrt.js'
+import cors from 'cors'
 
 const app = express()
 
 app.disable('x-powered-by') // Deshabilitar el headear X-Powered-By: Express
 
 app.use(json())
+app.use(cors())
 
 const PORT = process.env.PORT ?? 3000
+
+// Handle CORS for all routes
+app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+    res.send()
+})
 
 app.get('/', (req, res) => {
     res.json({ message: 'Bienvenido a ITS food' })
